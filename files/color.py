90ca9a392493d4a25117677ca3d625e04d5941be
Abhinav Shrivastava <abhinav.ranger@gmail.com>
2021-09-23 09:21:47 +0530

    added color

---
+++ files/color.py
@@ -0,0 +1,16 @@
+import random
+
+async def get_color() -> int:
+    """
+    Generates a random hexadecimal color.
+    Returns:
+        color: int - a hexadecimal number
+    """ 
+
+    # Generate a random color
+    color = "06x" % random.randint(0, 0xFFFFFF)
+
+    # Convert to int for it to work
+    color = int(color, 16)
+
+    return color

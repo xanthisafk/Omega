491296dbf81b1c5a47a14c8e5e99ab35cfb0f103
Abhinav <abhinav.ranger@gmail.com>
2021-10-10 17:25:14 +0530

    Command to fetch avatar of a given user

---
+++ cogs/avatar.py
@@ -0,0 +1,26 @@
+import discord
+import APIs.color as rang
+from discord.ext import commands
+
+
+class Avatar(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['av'])
+    async def avatar(self, ctx, user: discord.Member = None):
+        color = await rang.get_color()
+        if user == None:
+            nm = ctx.author.name + " 's avatar."
+            embed = discord.Embed(title=nm, color=color)
+            embed.set_image(url=ctx.author.avatar_url)
+        
+        else:
+            nm = user.name + " 's avatar."
+            embed = discord.Embed(title=nm, color=color)
+            embed.set_image(url=user.avatar_url)
+        
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))

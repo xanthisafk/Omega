1489e7b15b9d92ba6a5d46b52a6a2d1ba12e28de
Abhinav <abhinav.ranger@gmail.com>
2021-10-10 17:25:52 +0530

    Fixed some typos and formatted it to suit better for my liking

--- cogs/emotes.py
+++ cogs/emotes.py
@@ -1,31 +1,24 @@
-import discord
-from discord.ext import commands
-import json, codecs
-import loggers.logger as log
+import codecs
+import json
+
 import APIs.color as rang
 import APIs.GIF_And_Text
+import discord
+import loggers.logger as log
+from discord.ext import commands
+
 
 class Emotes(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.cog_name = __name__[5:].capitalize()
         self.gif = APIs.GIF_And_Text.GIF_And_Text()
+    
+    aliases = ['dance', 'wave', 'sleep', 'vibe', 'pat', 'cry', 'pout', 'kiss', 'bully', 'hug', 'cuddle', 'lick', 'smug', 'bonk', 'yeet', 'throw', 'smile', 'happy', 'highfive', 'handhold', 'hold', 'eat', 'hungry', 'bite', 'glomp', 'superhug', 'slap', 'kill', 'kick', 'wink', 'poke', 'cringe', 'baka', 'hmph', 'bored', 'facepalm', 'feed', 'laugh', 'shrug', 'stare', 'think', 'thonk', 'thumbsup', 'tickle'] 
 
-    @commands.command(name='hungry', aliases=[
-        'blush',    'kiss',     'smile',    'bite',
-        'bully',    'throw',    'nom',      'cringe',
-        'wave',     'cuddle',   'happy',    'glomp',
-        'hug',      'highfive', 'superhug', 'poke',
-        'lick',     'handhold', 'slap',     'wink',
-        'pat',      'smug',     'hold',     'kill',
-        'cry',      'bonk',     'eat',      'kick',
-        'yeet',     'dance',    'sleep',    'vibe',
-        'pout',     'baka',     'hmph',     'bored',
-        'facepalm', 'feed',     'shrug',    'stare',
-        'think',    'thumbsup', 'tickle',   'thonk'
-    ])
-    async def send_emotes(self, ctx, member:discord.Member=None):
-        
+    @commands.command(name='blush', aliases=aliases)
+    async def send_emotes(self, ctx, member: discord.Member = None):
+
         name = ctx.invoked_with
         color = await rang.get_color()
 
@@ -41,7 +34,7 @@ class Emotes(commands.Cog):
                 solo = True
         else:
             solo = False
-        
+
         category = alias['emotes'][name]['cate']
 
         alias = None
@@ -50,17 +43,18 @@ class Emotes(commands.Cog):
 
         if not solo:
             u2 = member.name
-            string = await self.gif.create_string(type=name,user1=u1,user2=u2)
+            string = await self.gif.create_string(type=name, user1=u1, user2=u2)
         else:
-            string = await self.gif.create_string(type=name,user1=u1)
+            string = await self.gif.create_string(type=name, user1=u1)
 
         image = await self.gif.selector(category)
 
-        embed = discord.Embed(title = string, color = color)
+        embed = discord.Embed(title=string, color=color)
         embed.set_image(url=image)
         await ctx.send(embed=embed)
-        await log.event_logger(ctx,name,self.cog_name)
- 
+        await log.event_logger(ctx, name, self.cog_name)
+
 
 def setup(client):
     client.add_cog(Emotes(client))
+

f693d7fe1b00203b06c362abf16d58adf339156f
Abhinav <abhinav.ranger@gmail.com>
2021-10-10 17:26:24 +0530

    Added snipe command

---
+++ cogs/snipe.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import APIs.color as rang
+
+
+class Snipe(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    msg = None
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+
+        global msg
+        msg = message
+
+    @commands.command()
+    async def snipe(self, ctx):
+
+        global msg
+
+        color = await rang.get_color()
+
+        try:
+            thumb = msg.author.avatar_url
+        except Exception as e:
+            if isinstance(e,NameError):
+                await ctx.send("There is nothing to snipe...")
+                return
+
+        name = msg.author.name
+
+        embed = discord.Embed(description=msg.content, color = color)
+        embed.set_author(name=name , icon_url=thumb)
+
+        if msg.attachments == []:
+            pass
+        else:
+            embed.set_image(url=(msg.attachments[0].url))
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Snipe(bot))
